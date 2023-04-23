6faf9fba047ebdd3a967a6c605e29da9920668bd
Jared Hollingsworth <123146317+jared2468@users.noreply.github.com>
2023-04-23 11:01:48 -0700

    2nd Commit

--- WeekJavascript.js
+++ WeekJavascript.js
@@ -1,15 +1,19 @@
 
-let addBtn = document.getElementById('add_btn');
-addBtn.addEventListener('click', addSong);
-let parentList = document.getElementById('parent-List');
+let addBtn = document.getElementById('add_btn'); // variable to hold the element for add_btn
+addBtn.addEventListener('click', addSong); // Event listener to add a song when button is clicked
+let parentList = document.getElementById('parent-List'); // creates a variable for the parent list
+
+// function to add a song to the setlist app
 function addSong(e) {
     if (parentList.children.length > 0 && parentList.children[0].className == "emptyMsg") {
         parentList.children[0].remove();
     }
-    let currentBtn = e.currentTarget;
-    let currentInput = currentBtn.previousElementSibling;
-    let currentSongName = currentInput.value;
+    let currentBtn = e.currentTarget; 
+    let currentInput = currentBtn.previousElementSibling; 
+    let currentSongName = currentInput.value; 
     currentInput.value = "";
+
+    // if loop to handle empty form 
     if (currentSongName.length <= 0) {
         let newEmptyMsg = document.createElement("h5");
         newEmptyMsg.classList.add("emptyMsg");
@@ -17,6 +21,7 @@ function addSong(e) {
         parentList.appendChild(newEmptyMsg);
        
     }
+    // handles a string from the input and renders the DOM
     else {
         let newLi = document.createElement('li');
         // newLi.classList.add('list-group-item');
@@ -32,7 +37,8 @@ function addSong(e) {
     
 }
 
-function removeSong(currElement) {
+// handles the delete function
+function removeSong(currElement) { 
     currElement.parentElement.remove();
     if (parentList.children.length <= 0) {
         let newEmptyMsg = document.createElement("h5");
@@ -42,6 +48,7 @@ function removeSong(currElement) {
     }
 }
 
+// edits current element 
 function editSong(currElement) {
     if (currElement.textContent == "Done") {
         currElement.textContent = "Edit";
